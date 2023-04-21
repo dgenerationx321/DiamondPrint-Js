fef1d14176022e1b0a028eb920c7f0090d9ac0bd
dgenerationx321 <kumardeepaksah37@gmail.com>
2023-04-22 01:15:35 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let n = 5;
+let string = "";
+// Upside Portion
+for (let i = 1; i <= n; i++) {
+  //    printing spaces
+  for (let j = n; j > i; j--) {
+    string = string + " ";
+  }
+  // printing star
+  for (let k = 0; k < i * 2 - 1; k++) {
+    string = string + "*";
+  }
+  string = string + "\n";
+}
+// downside portion
+for (let i = 1; i <= n - 1; i++) {
+  // priniting spaces
+  for (let j = 0; j < i; j++) {
+    string = string + " ";
+  }
+  // printing star
+  for (let k = (n - i) * 2 - 1; k > 0; k--) {
+    string = string + "*";
+  }
+  string = string + "\n";
+}
+console.log(string);
